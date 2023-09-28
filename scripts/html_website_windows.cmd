e4a5ffbe39a2b4ee7d46c8546d21dce71f4db99e
Zoda <73388608+kerem3338@users.noreply.github.com>
2023-09-28 18:41:49 +0300

    Update html_website_windows.cmd
    Test #4

--- scripts/html_website_windows.cmd
+++ scripts/html_website_windows.cmd
@@ -1,11 +1,11 @@
 @echo off
 (
-echo <html>
-echo <head>
-echo <title>HTML WEBSITE</title>
-echo </head>
-echo <body>
-echo <p>Hello World</p>
-echo </body>
-echo </html>
+echo ^<html^>
+echo ^<head^>
+echo ^<title^>HTML WEBSITE^</title^>
+echo ^</head^>
+echo ^<body^>
+echo ^<p^>Hello World^</p^>
+echo ^</body^>
+echo ^</html^>
 ) > index.html
