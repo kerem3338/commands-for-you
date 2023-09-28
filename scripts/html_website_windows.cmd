0780c2db5bf4c4ca7a8c67c8003732880ea67612
Zoda <73388608+kerem3338@users.noreply.github.com>
2023-09-28 18:58:18 +0300

    Update html_website_windows.cmd
    Test #5

--- scripts/html_website_windows.cmd
+++ scripts/html_website_windows.cmd
@@ -3,9 +3,19 @@
 echo ^<html^>
 echo ^<head^>
 echo ^<title^>HTML WEBSITE^</title^>
+echo ^<link rel="stylesheet" href="styles.css"^>
 echo ^</head^>
 echo ^<body^>
 echo ^<p^>Hello World^</p^>
 echo ^</body^>
+echo ^<script src="script.js" ^>^<script^>
 echo ^</html^>
 ) > index.html
+
+(
+echo console.log^("hello world"^)
+) > script.js
+
+(
+echo body {background-color:red}
+) > styles.css
